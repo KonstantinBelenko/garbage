c2852139b2f844a7195e624de8ee6146fd4a9693
Kostiantyn <konstantinbelenko36@gmail.com>
2023-12-15 15:46:49 +0100

    removed rust folder :D

--- target/debug/deps/lazy_static-07533272ed792c85.d
+++
@@ -1,6 +0,0 @@
-/Users/db/Desktop/SideProjects/compilers/garbagers/target/debug/deps/lazy_static-07533272ed792c85.rmeta: /Users/db/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /Users/db/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
-
-/Users/db/Desktop/SideProjects/compilers/garbagers/target/debug/deps/lazy_static-07533272ed792c85.d: /Users/db/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs /Users/db/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs
-
-/Users/db/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/lib.rs:
-/Users/db/.cargo/registry/src/index.crates.io-6f17d22bba15001f/lazy_static-1.4.0/src/inline_lazy.rs:
